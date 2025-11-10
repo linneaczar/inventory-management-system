2c8d9f5cdfc1300dab272cb78b7774ad98b63151
linneaczar <linnea.czarnecki@gmail.com>
2025-11-10 08:36:01 +0100

    Added GET-endpoints

--- main.js
+++ main.js
@@ -169,7 +169,35 @@ app.post("/products", (req, res) => {
 
 });
 
+//GET/products - Hämta alla produkter
+//=======================================
 
+app.get("/products", (req, res) => {
+  res.json(products);
+}); 
+
+//GET/products/:id - Hämta en specifik produkt
+//=======================================
+
+app.get("/products/:id", (req, res) => {
+const id = Number.parseInt(req.params.id);
+
+// Valdidering: Om användaren skriver in något som inte är ett nummer
+if (Number.isNaN(id)) {
+  res.status(400).json({ error: "Id param must be a number"});
+  return;
+};
+
+//Söker i arrayen efter en produkt med matchande id
+const product = product.find(all => all.id === id);
+if (product === undefined) {
+  res.status(404).json({ error: "Product with id not found"});
+  return;
+};
+
+res.json(product);
+
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
